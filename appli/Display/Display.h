afe018b80bee24402015577f9016bcecc1ec3866
BrigitteLPB <guillemaud.theo@gmail.com>
2020-09-22 13:30:02 +0200

    ajout des fichiers de structuration

---
+++ appli/Display/Display.h
@@ -0,0 +1,13 @@
+/*
+ * Display.h
+ *
+ *  Created on: 22 sept. 2020
+ *      Author: guill
+ */
+
+#ifndef DISPLAY_DISPLAY_H_
+#define DISPLAY_DISPLAY_H_
+
+	// single include for displaying the game
+
+#endif /* DISPLAY_DISPLAY_H_ */
---
+++ appli/Logical/Logical.h
@@ -0,0 +1,14 @@
+/*
+ * Logical.h
+ *
+ *  Created on: 22 sept. 2020
+ *      Author: guill
+ */
+
+#ifndef LOGICAL_LOGICAL_H_
+#define LOGICAL_LOGICAL_H_
+
+	// single include for the game logic
+
+
+#endif /* LOGICAL_LOGICAL_H_ */
