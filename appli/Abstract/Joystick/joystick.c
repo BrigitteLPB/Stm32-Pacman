b77eb9cd6f5fbfc9d07711ec6e95652c31f01cf2
BrigitteLPB <guillemaud.theo@gmail.com>
2020-10-05 14:33:49 +0200

    joystick add files

---
+++ appli/Abstract/Joystick/joystick.c
@@ -0,0 +1,17 @@
+/**
+ * @file	joystick.c
+ * @brief	Definition for abstract module for a module
+ * @author	Théo Guillemaud
+ */
+#include "joystick.h"
+
+// include
+
+
+// const
+
+
+// private function
+
+
+// function
---
+++ appli/Abstract/Joystick/joystick.h
@@ -0,0 +1,18 @@
+/**
+ * @file	joystick.h
+ * @brief	All declaration to abstract a Joystick
+ * @author	Théo Guillemaud
+ */
+#ifndef JOYSTICK_H_
+	#define JOYSTICK_H_
+
+	// include
+
+
+	// const
+
+
+	// functions
+
+
+#endif
