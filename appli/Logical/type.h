c8cf75f88ae3ce872123760a96947c14897e783f
BrigitteLPB <57198764+BrigitteLPB@users.noreply.github.com>
2020-10-13 14:36:13 +0200

    Update type.h
    rectify types

--- appli/Logical/type.h
+++ appli/Logical/type.h
@@ -2,7 +2,7 @@
  * type.h
  *
  *  Created on: 6 Oct 2020
- *      Author: Théo Malinge--Lecompte
+ *      Author: ThÃ©o Malinge--Lecompte
  */
 
 #ifndef LOGICAL_TYPE_H_
@@ -12,58 +12,82 @@
 	#include "stdbool.h"
 
 //IA state
-#define LENGTH 320
-#define HEIGHT 240
+	#define LENGTH 32	// largeur de la map
+	#define	HEIGHT 24	// hauteur de la map
 
-	typedef enum{
-		ALIVE,
-		DEAD,
-		PREDATOR		//under gumball effect
-	}type_state_pacman;
+	#define MAX_PHANTOM	4	// maximum on the map
 
-	typedef enum{
-		LIBERATE,
-		CAPTIVE,
-		PREY			//under gumball effect
-	}type_state_FANTOME;
+	typedef PACAMAN_unit uint8_t;
 
-	typedef struct{
-		type_state_pacman pacman;
-		type_state_FANTOME fantome;
-	}IA_state;
+	typedef  struct{
+		PACMAN_unit x;
+		PACMAN_unit y;
+	}PACMAN_position;
 
 //state cell
+	// cell
 	typedef enum{
 		WALL,		//cell where we have a wall
+		WALL_WITH_PHANTOM,	// phantom spawn area
 		FREE,		//cell with nothing on it
 		OBJECT		//cell with a point on it
 	}type_cell;
 
+	typedef struct {
+		void *data;
+		type_cell type;
+	}cell_s;
+
+	// object / caracter
 	typedef enum{		//OBJECT
 		PACMAN,
-		FANTOME,
+		PHANTOM,
 		GUMBALL,
 		CHERRY,
 		POINT
 	}type_object;
 
 	typedef struct {
-		void *cell;
-		type_cell square;
-		type_object object;
-	}cell_s;
+		void* data;
+		type_object type;
+	}object_s;
 
+// ojects
+	// pacman
+	typedef enum{
+		ALIVE,
+		DEAD,
+		PREDATOR		//under gumball effect
+	}type_state_pacman;
+
+	typedef struct {
+		PACMAN_position pos;
+		uint8_t score;
+		type_state_pacman state;
+	}pacman_s;
+
+	// phantom
+	typedef enum{
+		LIBERATE,
+		CAPTIVE,
+		PREY			//under gumball effect
+	}type_state_PHANTOM;	
+
+	typedef struct {
+		PACMAN_positon pos;
+		type_state_PHANTOM state;
+		int8_t timer;		// 0: move, -1: stay, >0: wait for moving
+	}phantom_s;
+
+
+// map
 	typedef struct{
-		IA_state ia;
-		cell_s display;
-		int Length[LENGTH];
-		int Heigth[HEIGHT];
+		uint8_t phantom_count;
+		uint8_t points_count;	// 0 -> win
+		cell_s map[HEIGTH][LENGTH];
+		phantom_s *phantoms[MAX_PHANTOM];
+		pacman_s *pacman;
 	}game_s;
-
-
-
-
-
 
 
 
