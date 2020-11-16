be7eae6cdfb632e58723e4ef68356ce739cd320e
BrigitteLPB <guillemaud.theo@gmail.com>
2020-11-16 17:07:29 +0100

    menu fini

---
+++ appli/Display/menu.c
@@ -0,0 +1,34 @@
+#include "Display/menu.h"
+
+/*	typedef enum
+	{
+		JEU,
+		MENU
+	}state_e;*/
+
+
+void menu(){
+	static state_game state = MENU;
+	switch(state){
+		case MENU:
+			if(JOYSTICK_getDirection(JOYSTICK2)== DROITE){
+				initMAP();
+				refreshMAP();
+				state = JEU;
+			}
+			break;
+		case JEU:
+			state = jeu();
+			if (state == MENU){
+				initMenu();
+			}
+			break;
+	}
+
+}
+
+void initMenu(){
+	ILI9341_DrawFilledRectangle((uint16_t)(0),(uint16_t)(0),(uint16_t)(320),(uint16_t)(240),ILI9341_COLOR_BLACK);
+	TFT_text_s text = TFT_make_text("New game", (pos_s){60,140}, FONT_16x26, COLOR_WHITE, COLOR_BLACK);
+	TFT_put_text(&text);
+}
