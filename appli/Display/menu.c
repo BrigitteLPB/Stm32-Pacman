77fc46d6bab9c46dc520cc27bdb9ed2d2daf8c73
BrigitteLPB <guillemaud.theo@gmail.com>
2020-11-24 18:27:35 +0100

    merge menu et hard mode

--- appli/Display/menu.c
+++ appli/Display/menu.c
@@ -9,6 +9,8 @@ static uint16_t score = 0;
 
 void menu(){
 	static state_game state = INIT;
+	static bool_e bas = FALSE;
+
 	switch(state){
 		case INIT:
 			state = MENU;
@@ -16,8 +18,9 @@ void menu(){
 			break;
 
 		case MENU:
+
 			if(JOYSTICK_getDirection(JOYSTICK2)== DROITE || JOYSTICK_getDirection(JOYSTICK1) == DROITE){
-				LOGICAL_init(TRUE);
+				LOGICAL_init(bas);
 				state = JEU;
 			}
 			else if (JOYSTICK_getDirection(JOYSTICK2)== BAS || JOYSTICK_getDirection(JOYSTICK1) == BAS){
@@ -25,12 +28,16 @@ void menu(){
 				TFT_put_text(&text3);
 				TFT_text_s text4 = TFT_make_text("->", (pos_s){100,100}, FONT_16x26, COLOR_WHITE, COLOR_BLACK);
 				TFT_put_text(&text4);
+
+				bas = TRUE;
 			}
 			else if (JOYSTICK_getDirection(JOYSTICK2)== HAUT || JOYSTICK_getDirection(JOYSTICK1) == HAUT){
 				TFT_text_s text3 = TFT_make_text("->", (pos_s){60,100}, FONT_16x26, COLOR_WHITE, COLOR_BLACK);
 				TFT_put_text(&text3);
 				TFT_text_s text4 = TFT_make_text("  ", (pos_s){100,100}, FONT_16x26, COLOR_WHITE, COLOR_BLACK);
 				TFT_put_text(&text4);
+
+				bas = FALSE;
 			}
 			break;
 
@@ -39,9 +46,11 @@ void menu(){
 			if (state == MENU){
 				LOGICAL_kill();
 				initMenu();
+				bas = FALSE;
 			}else if(state == WIN){
 				LOGICAL_kill();
 				winMenu();
+				bas = FALSE;
 			}
 			break;
 
